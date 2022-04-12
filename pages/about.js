57d08488fb20e7c9a722964cc8126b8a989af1fb
DongJuneShin <DongJuneShin@gmail.com>
2022-04-12 22:44:46 +0900

    연락처 추가 및 공통상단

--- pages/about.js
+++ pages/about.js
@@ -4,17 +4,12 @@ export default function About() {
   return (
     <>
       <Head>
-        <title>앱소개 | 투두</title>
-        <meta name="description" content="할일관리서비스 투두 입니다." />
-        <link rel="icon" href="/favicon.ico" />
+      
       </Head>
 
       <h1>앱 소개 페이지</h1>
 
-      <nav>
-        <a href="/">메인</a>
-        <a href="/about" style={{color:'red'}}>어바웃</a>
-      </nav>
+     
     </>
-  )
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -7,16 +7,10 @@ export default function Home() {
     <>
       <Head>
         <title>메인 | 투두</title>
-        <meta name="description" content="할일관리서비스 투두 입니다." />
-        <link rel="icon" href="/favicon.ico" />
+        
       </Head>
-
       <h1>메인 페이지</h1>
-
-      <nav>
-        <a href="/" style={{color:'red'}}>메인</a>
-        <a href="/about">어바웃</a>
-      </nav>
+     
     </>
-  )
+  );
 }
