34f4641c293e2cf6996f105e25d39fe1e4944489
DongJuneShin <DongJuneShin@gmail.com>
2022-04-13 22:26:24 +0900

    mui추가

--- pages/about.js
+++ pages/about.js
@@ -1,13 +1,22 @@
-import Head from 'next/head'
+import { AppBar, Toolbar } from "@mui/material";
+import Head from "next/head";
+import ToolbarContent from "../components/ToolbarContent";
 
 export default function About() {
   return (
     <>
       <Head>
-      
+        <title>앱소개 | 투두</title>
       </Head>
 
-      <h1>앱 소개 페이지</h1>
+      <AppBar position="fixed">
+        <Toolbar>
+          <ToolbarContent></ToolbarContent>
+        </Toolbar>
+      </AppBar>
+      <Toolbar />
+
+      <h1 className="mt-3">어바웃 페이지</h1>
 
      
     </>
